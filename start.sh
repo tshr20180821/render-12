1fc7940d13f3a5cfe8a123241234b9399ca389d5
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-13 10:52:37 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
 sed -i s/__RENDER_EXTERNAL_HOSTNAME__/"${RENDER_EXTERNAL_HOSTNAME}"/g /etc/apache2/sites-enabled/apache.conf
 
-. /etc/apache2/envvars >/dev/null 2>&1
+. /etc/apache2/envvars
 
 curl -sSLO https://raw.githubusercontent.com/tshr20180821/render-12/main/start_after.sh?"$(date +%s)"
 
