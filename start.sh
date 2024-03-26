693afff1f7efc0e69ea527ced3f7400143c57bcd
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-27 07:00:02 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ set -x
 
 export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
+cat /proc/version
+cat /etc/os-release
+
 sed -i s/__RENDER_EXTERNAL_HOSTNAME__/"${RENDER_EXTERNAL_HOSTNAME}"/g /etc/apache2/sites-enabled/apache.conf
 
 . /etc/apache2/envvars
